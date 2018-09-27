5ac2bec26d1e564dfd23abe19274a2bebed0eadc
cat-vandal <43626909+cat-vandal@users.noreply.github.com>
2018-09-27 10:45:01 -0700

    TEsting

--- helloCat.js
+++ helloCat.js
@@ -1,1 +1,5 @@
-http://thecatapi.com/api/images/get?format=src&type=gif
+if(Math.floor(Math.random() * 2)==1){
+  return("http://thecatapi.com/api/images/get?format=src&type=gif")
+} else {
+  return("TEST")
+}
